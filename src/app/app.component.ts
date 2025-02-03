eb9f99f2bcc5a85cea2b509f5e628da741f269fe
Omar Trabelsi <omar.trabelsi.fst@gmail.com>
2025-02-03 05:33:12 +0100

    adding missing files.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable, interval, startWith, switchMap } from 'rxjs';
+
+import { ExchangeRate } from './model/exchange-rate.model';
+import { ExchangeRateService } from './services/exchange-rate.service';
+import { MenuComponent } from './menu/menu.component';
+import { ConversionComponent } from './conversion/conversion.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MenuComponent,
+    ConversionComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'exchange-rates-ui';
+export class AppComponent implements OnInit {
+  exchangeRate$!: Observable<ExchangeRate>;
+
+  constructor(private exchangeRateService: ExchangeRateService) {}
+
+  ngOnInit() {
+    // On appelle l'API toutes les 2 secondes
+    this.exchangeRate$ = interval(2000).pipe(
+      startWith(0),
+      switchMap(() => this.exchangeRateService.getExchangeRate())
+    );
+  }
 }
